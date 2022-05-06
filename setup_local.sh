e464cf9d5f1b305c476ef247274ea827ec78c260
AlessandroLovoENSL <alessandro.lovo@ens-lyon.fr>
2022-05-06 15:04:55 +0200

    Better setup_local

--- setup_local.sh
+++ setup_local.sh
@@ -7,6 +7,7 @@
 # -----------------------------
 
 jnr_path="$HOME/.RemoteJupyter"
+version="0.3.2"
 
 usage () {
     echo "Usage"
@@ -33,16 +34,19 @@ while getopts ":ha:" opt; do
 done
 
 # set default alias if not provided
-if [[ -z "$jnr_alias" ]]; then
+if [[ -z "$jnr_alias" ]] ; then
     jnr_alias="jnr"
 fi
 
-# check if given alias already exists
-if command -v "$jnr_alias" &> /dev/null ; then
-    echo "command $jnr_alias is already in use"
-    echo "Please specify another alias for RemoteJupyter by running"
-    echo "    source setup_local.sh -a <alias>"
-    return 1
+# check if there is already a setup file, which means we just need to update
+if [[ ! -f "$jnr_path/setup.txt" ]] ; then
+    # check if given alias already exists
+    if command -v "$jnr_alias" &> /dev/null ; then
+        echo "command $jnr_alias is already in use"
+        echo "Please specify another alias for RemoteJupyter by running"
+        echo "    source setup_local.sh -a <alias>"
+        return 1
+    fi
 fi
 
 # identify the type of shell
@@ -65,7 +69,7 @@ fi
 
 # create a setup file
 setup_date=`date`
-echo "$jnr_alias~$setup_date" > "$jnr_path/setup.txt"
+echo "$jnr_alias~$setup_date --- version=$version" >> "$jnr_path/setup.txt"
 
 # copy jnr.sh into jnr_path
 cp jnr.sh "$jnr_path"
@@ -76,12 +80,36 @@ chmod u+x "$jnr_path/jnr.sh"
 # make a copy of the original rc file
 cp "$rc_file" "$rc_file.pre_jnr"
 
-# create alias in rc file
-echo >> "$rc_file"
-echo "# >> RemoteJupyter" >> "$rc_file"
-echo "alias $jnr_alias='$jnr_path/jnr.sh'" >> "$rc_file"
-echo "# << RemoteJupyter" >> "$rc_file"
-echo >> "$rc_file"
+# read the rc file and remove the old region where the aliases are defined
+found=false
+copy=true
+while IFS='' read -r line ; do
+    if [[ "$line" == *RemoteJupyter ]] ; then
+        if [[ "$copy" == true ]] ; then
+            copy=false
+            found=true
+        else
+            copy=true
+            echo "# >> RemoteJupyter" >> "$rc_file.tmp"
+            echo "alias $jnr_alias='$jnr_path/jnr.sh'" >> "$rc_file.tmp"
+        fi
+    fi
+
+    if [[ "$copy" == true ]] ; then
+        echo "$line" >> "$rc_file.tmp"
+    fi
+    
+done < "$rc_file"
+mv "$rc_file.tmp" "$rc_file"
+
+if [[ "$found" == false ]] ; then
+    # create alias in rc file
+    echo >> "$rc_file"
+    echo "# >> RemoteJupyter" >> "$rc_file"
+    echo "alias $jnr_alias='$jnr_path/jnr.sh'" >> "$rc_file"
+    echo "# << RemoteJupyter" >> "$rc_file"
+    echo >> "$rc_file"
+fi
 
 # Tell the user how to proceed
 echo
